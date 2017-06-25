fdc0231d6bb39195ae671cb6557735baaf236a61
seanksullivan <seanksullivan@gmail.com>
2017-06-25 13:54:05 -0700

    The purpose of this MSTest unit test project is to demonstrate that MSTest2 unit tests are very similar to nUnit tests. Both frameworks have their strengths and weaknesses - fyi: my comfor zone is MSTest2, but I appreciate some of the functionality that nUnit has to offer .

--- ILMergeMsTest.UnitTests/KeyMSTests.cs
+++ ILMergeMsTest.UnitTests/KeyMSTests.cs
@@ -5,60 +5,117 @@ using ILMerging;
 using System.Reflection;
 using TestTools.Helpers;
 using TestTools;
+using System.Security.Cryptography;
+using System.Collections.Generic;
 
 namespace ILMergeMsTest.UnitTests
 {
+    /// <summary>
+    /// The purpose of this class is to demonstrate that MSTest/MSTest2 is just as simple to use as nUnit.
+    /// MSTest and nUnit both have their own strengths and weeknesses, but for the most part - these tests are simple.
+    /// </summary>
     [TestClass]
     public class KeyMSTests
     {
+        #region Private Members
+        private const string Extension = ".dll";
+        private const string TestSnk = "test.snk";
+        private const string TestPfx = "test.pfx";
+        private static List<string> _keyContainerNameList = new List<string>();
+        #endregion
+
+        #region Public Properties
         public TestContext TestContext { get; set; }
+        #endregion
+
+        #region Init and Cleanup
+        /// <summary>
+        /// Let's cleanup Csp Containers after all tests have executed (successfully or unsuccessfully).
+        /// </summary>
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            _keyContainerNameList.ForEach(containerName => CspContainerUtils.Delete(true, containerName, KeyNumber.Signature));
+        }
+        #endregion
 
         [TestMethod]
         [DeploymentItem("TestData")]
         public void Can_sign_using_keyfile()
         {
-            const string Extension = ".dll";
-            const string TestSnk = "test.snk";
-
-            var outputFile = Path.Combine(TestContext.DeploymentDirectory, Path.ChangeExtension(Path.GetRandomFileName(), Extension));
+            // ARRANGE
+            var outputFile = Path.Combine(TestContext.DeploymentDirectory, 
+                Path.ChangeExtension(Path.GetRandomFileName(), Extension));
 
             var ilMerge = new ILMerge { KeyFile = "test.snk", OutputFile = outputFile };
+
             ilMerge.SetUpInputAssemblyForTest(Assembly.GetExecutingAssembly());
+
+            // ACT
             ilMerge.Merge();
 
+            // ASSERT
             var expectedKeyBytes = new StrongNameKeyPair(File.ReadAllBytes(TestSnk)).PublicKey;
-            var outputFileKeyBytes = AssemblyName.GetAssemblyName(outputFile).GetPublicKey();
-            
+            var outputFileKeyBytes = AssemblyName.GetAssemblyName(outputFile).GetPublicKey();          
             CollectionAssert.AreEqual(expectedKeyBytes, outputFileKeyBytes, "Expected PublicKey bytes do not match-up to the generated assembly PublicKey bytes");
         }
 
-        //[TestMethod]
-        //[DeploymentItem("TestData")]
-        //public void Can_sign_using_keycontainer()
-        //{
-        //    var keyContainerName = Guid.NewGuid().ToString();
-        //    CspContainerUtils.ImportBlob(true, keyContainerName, KeyNumber.Signature, File.ReadAllBytes(TestFiles.TestSnk));
-        //    try
-        //    {
-        //        using (var outputFile = TempFile.WithExtension(".dll"))
-        //        {
-        //            var ilMerge = new ILMerge
-        //            {
-        //                KeyContainer = keyContainerName,
-        //                OutputFile = outputFile
-        //            };
-        //            ilMerge.SetUpInputAssemblyForTest(Assembly.GetExecutingAssembly());
-        //            ilMerge.Merge();
+        [TestMethod]
+        [DeploymentItem("TestData")]
+        public void Can_sign_using_keycontainer()
+        {
+            // ARRANGE
+            var keyContainerName = Guid.NewGuid().ToString();
+            _keyContainerNameList.Add(keyContainerName);
+            
+            CspContainerUtils.ImportBlob(true, keyContainerName, KeyNumber.Signature, File.ReadAllBytes(TestSnk));
 
-        //            Assert.That(
-        //                AssemblyName.GetAssemblyName(outputFile).GetPublicKey(),
-        //                Is.EqualTo(new StrongNameKeyPair(File.ReadAllBytes(TestFiles.TestSnk)).PublicKey));
-        //        }
-        //    }
-        //    finally
-        //    {
-        //        CspContainerUtils.Delete(true, keyContainerName, KeyNumber.Signature);
-        //    }
-        //}
+            var outputFile = Path.Combine(TestContext.DeploymentDirectory, 
+                Path.ChangeExtension(Path.GetRandomFileName(), Extension));
+
+            var ilMerge = new ILMerge
+            {
+                KeyContainer = keyContainerName,
+                OutputFile = outputFile
+            };
+            ilMerge.SetUpInputAssemblyForTest(Assembly.GetExecutingAssembly());
+
+            // ACT
+            ilMerge.Merge();
+
+            // ASSERT
+            var expectedKeyBytes = new StrongNameKeyPair(File.ReadAllBytes(TestSnk)).PublicKey;
+            var outputFileKeyBytes = AssemblyName.GetAssemblyName(outputFile).GetPublicKey();
+            CollectionAssert.AreEqual(expectedKeyBytes, outputFileKeyBytes, "Expected PublicKey bytes do not match-up to the generated assembly PublicKey bytes");
+        }
+
+        [TestMethod]
+        [DeploymentItem("TestData")]
+        public void Bad_keyfile_gives_diagnostic_warning()
+        {
+            // ARRANGE
+            var outputFile = Path.Combine(TestContext.DeploymentDirectory,
+                Path.ChangeExtension(Path.GetRandomFileName(), Extension));
+
+            var logFile = Path.Combine(TestContext.DeploymentDirectory,
+                Path.ChangeExtension(Path.GetRandomFileName(), ".log"));
+
+            var ilMerge = new ILMerge
+            {
+                KeyFile = TestPfx,
+                OutputFile = outputFile,
+                LogFile = logFile
+            };
+            ilMerge.SetUpInputAssemblyForTest(Assembly.GetExecutingAssembly());
+
+            //ACT
+            ilMerge.Merge();
+
+            //ASSERT
+            var logText = File.ReadAllText(logFile);
+            Assert.IsTrue(logText.Contains("Unable to obtain public key for StrongNameKeyPair."));
+            Assert.IsTrue(logText.Contains("PFX"));
+            Assert.IsTrue(logText.Contains("key container"));
+        }
     }
 }
---
+++ ILMergeMsTest.UnitTests/Readme.txt
@@ -0,0 +1,17 @@
+﻿The purpose of this unit test project is to demonstrate that MSTest2 unit tests are very similar to nUnit tests.
+Both frameworks have their strengths and weaknesses - but I prefer MSTest2 :).
+
+I do appreciate nUnit's TestFixture, and TestClass functionality - 
+	I think MSTest2 can provide the same benefits; in some cases in a simpler manner, other cases... not so simple.
+
+With MSTest2 we can use the DataRow attribute, which is similar to nUnit's TestCase attribute - to provide data-driven unit test processing.
+
+nUnit example:
+        [TestCase(true, TestName = "{m}(with mscorsn in path)")]
+        [TestCase(false, TestName = "{m}(without mscorsn in path)")]
+        public void No_DLL_load_crashes_when_given_PFX(bool withMscorsnInPath)
+
+MSTest2 example:
+        [DataRow(true, DisplayName = "(with mscorsn in path)")]
+        [DataRow(false, DisplayName = "(without mscorsn in path)")]
+        public void No_DLL_load_crashes_when_given_PFX(bool withMscorsnInPath)
